ebe49a676e9da896b45ac6072ebce440a2ab83cf
Thiago <Thiago.a.rostomashvili@gmail.com>
2024-02-25 21:26:53 +0000

    Adicionei novas moedas e tbm ja iniciei a função do select de cima

--- scripts.js
+++ scripts.js
@@ -1,5 +1,13 @@
 const convertButton = document.querySelector(".convert-button");
 const currencySelect = document.querySelector(".currency-select");
+const currencySelectUp = document.querySelector(".currency-btn")
+
+function selectValues() {
+    const correncyValueConvert = document.querySelector(".currency-value-to-conver"); // valor em real
+
+    console.log(correncyValueConvert) // primeiro passo é pegar checar se a função ta chegando
+   
+}
 
 function convertValues() {
     const inputCurrencyValue = document.querySelector(".input-currency").value
@@ -7,8 +15,10 @@ function convertValues() {
     const correncyValueConverted = document.querySelector(".currency-value"); // valor em Dollar
 
     const dolarToday = 5.2
-    const euroToday = 6.2
-
+    const euroToday = 5.37
+    const realToday = 1
+    const ieneToday = 0.033
+    const libraToday = 6.33
 
     if (currencySelect.value == "dolar") { // (if)Se o select estiver selecionado o valor de dollar, entre aqui.
         correncyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
@@ -22,6 +32,24 @@ function convertValues() {
             currency: "EUR"
         }).format(inputCurrencyValue / euroToday)
     }
+    if (currencySelect.value == "real") { // (if)Se o select estiver selecionado o valor de Real, entre aqui.
+        correncyValueConverted.innerHTML = new Intl.NumberFormat("pt-br", {
+            style: "currency",
+            currency: "BRL"
+        }).format(inputCurrencyValue * realToday)
+    }
+    if (currencySelect.value == "iene") { // (if)Se o select estiver selecionado o valor de Iene, entre aqui.
+        correncyValueConverted.innerHTML = new Intl.NumberFormat("ja-JP", {
+            style: "currency",
+            currency: "JPY"
+        }).format(inputCurrencyValue * ieneToday)
+    }
+    if (currencySelect.value == "libra") {
+        correncyValueConverted.innerHTML = new Intl.NumberFormat("en-UK", {
+            style: "currency",
+            currency: "GBP"
+        }).format(inputCurrencyValue / libraToday)
+    }
 
     correncyValueToConvert.innerHTML = new Intl.NumberFormat("pt-br", {
         style: "currency",
@@ -29,7 +57,7 @@ function convertValues() {
     }).format(inputCurrencyValue)
 
 }
-function changeCurrency(){
+function changeCurrency() {
     const currencyName = document.getElementById("currency-name")
     const currencyImage = document.querySelector(".currency-img")
 
@@ -41,18 +69,27 @@ function changeCurrency(){
         currencyName.innerHTML = "Euro "
         currencyImage.src = "./assets/euro.png"
     }
-    if(currencySelect.value == "bit") {
+    if (currencySelect.value == "bit") {
         currencyName.innerHTML = "BitCoin"
         currencyImage.src = "./assets/bitcoin.png"
     }
-    if(currencySelect.value == "real") {
+    if (currencySelect.value == "real") {
         currencyName.innerHTML = "Real Brasileiro"
         currencyImage.src = "./assets/Real.png"
+    }
+    if (currencySelect.value == "iene") {
+        currencyName.innerHTML = "Iene"
+        currencyImage.src = "./assets/Iene.png"
+    }
+    if (currencySelect.value == "libra") {
+        currencyName.innerHTML = "Libra UK"
+        currencyImage.src = "./assets/libra.png"
     }
 
     convertValues()
 
 }
 
+
 currencySelect.addEventListener("change", changeCurrency)
-convertButton.addEventListener("click", convertValues);
+convertButton.addEventListener("click", convertValues)
